05ebb5ae0a4fa6dbb1b6da08fc06f4212317cfc2
pranali <pranalishendage0@gmail.com>
2026-03-14 15:59:35 +0530

    initial

---
+++ CharArray.java
@@ -0,0 +1,13 @@
+//write a java program to print allchracters in an array with its ASCII value.
+
+public class CharArray{
+public static void main(String []x){
+
+char []arr={'N','P','O','E','R','S','Y','T'};
+
+for(int i=0;i<arr.length;i++)
+ {
+  System.out.println(arr[i]+" "+(int)arr[i]+" ");
+  }
+ }
+}
