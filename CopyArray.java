c821b18a2164c66df39f30d18ccfd86477756f3b
Pranali Shendage <159568977+pranali-shendage@users.noreply.github.com>
2026-03-25 23:48:53 +0530

    Add files via upload

---
+++ CopyArray.java
@@ -0,0 +1,18 @@
+// Copy one array into another array
+
+public class CopyArray{
+public static void main(String []x){
+
+int []arr={1,2,3,4,5};
+int []copy=new int[arr.length];
+
+System.out.print("copy []:");
+for(int i=0;i<arr.length;i++)
+{
+  copy[i]=arr[i];  //copy element  arr[i] into copy[i].
+  
+   System.out.print(" "+copy[i]);
+}
+
+}
+}
